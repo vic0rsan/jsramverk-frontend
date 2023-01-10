dfa1ff00d8525b17a80ba6b46c61e5f5627d9840
gusu20 <gusu20@student.bth.se>
2023-01-10 12:31:56 +0100

    Fixed invite page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./components/auth";
-
-import Invite from "./components/invite";
-
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/invite" element={<Invite />} />
-      </Routes>
-    </BrowserRouter>
+    <Index />
   )
 }
